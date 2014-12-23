1a3b9df3e8511337a8b24183e2e053eb084c7022
Jae <jae@olapic.com>
2014-12-23 12:31:17 -0500

    yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ieToolkitApp
+ * @description
+ * # ieToolkitApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ieToolkitApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ieToolkitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ieToolkitApp
+ */
+angular.module('ieToolkitApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
